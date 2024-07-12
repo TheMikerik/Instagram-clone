5d106b7d2b0d03d89da47eecc65314bc0839cd44
TheMikerik <mchlrucka@gmail.com>
2024-07-12 12:59:37 +0200

    feed

--- lib/models/user.dart
+++ lib/models/user.dart
@@ -1,34 +1,23 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class User {
-  final String username;
-  final String uid;
   final String email;
+  final String uid;
+  final String photoUrl;
+  final String username;
   final String bio;
   final List followers;
   final List following;
-  final String photoUrl;
 
-  const User({
-    required this.email,
-    required this.uid,
-    required this.photoUrl,
-    required this.username,
-    required this.bio,
-    required this.followers,
-    required this.following,
-  });
+  const User(
+      {required this.username,
+      required this.uid,
+      required this.photoUrl,
+      required this.email,
+      required this.bio,
+      required this.followers,
+      required this.following});
 
-  Map<String, dynamic> toJson() => {
-        'username': username,
-        'uid': uid,
-        'email': email,
-        'bio': bio,
-        'followers': followers,
-        'following': following,
-        'photoUrl': photoUrl,
-      };
-  
   static User fromSnap(DocumentSnapshot snap) {
     var snapshot = snap.data() as Map<String, dynamic>;
 
@@ -43,4 +32,13 @@ class User {
     );
   }
 
+  Map<String, dynamic> toJson() => {
+        "username": username,
+        "uid": uid,
+        "email": email,
+        "photoUrl": photoUrl,
+        "bio": bio,
+        "followers": followers,
+        "following": following,
+      };
 }
--- lib/responsive/mobile_screen_layout.dart
+++ lib/responsive/mobile_screen_layout.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:instagram_clone/utils/colors.dart';
+import 'package:instagram_clone/utils/global_variables.dart';
 
 class MobileScreenLayout extends StatefulWidget {
   const MobileScreenLayout({super.key});
@@ -57,13 +58,7 @@ class _MobileScreenLayoutState extends State<MobileScreenLayout> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: PageView(
-        children: [
-          Text('feed'),
-          Text('search'),
-          Text('add'),
-          Text('likes'),
-          Text('profile'),
-        ],
+        children: homeScreenItems,
         physics: const NeverScrollableScrollPhysics(),
         controller: pageController,
         onPageChanged: onPageChanged,
